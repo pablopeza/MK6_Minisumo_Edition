3b923a82be125a699c45cd566f017db69b0bf0fa
pablopeza <pal16a@gmail.com>
2018-11-08 13:08:10 +0100

    soft

---
+++ Mk6_Sumo_edition/src/main.c
@@ -0,0 +1,52 @@
+// Made by Pablopeza for the Oshwdem2018 minisumo competition
+
+#include <Arduino.h>
+
+// Motor driver library by SparkFun
+#include <SparkFun_TB6612.h>
+
+// Sharp sensor library https://github.com/guillaume-rico/SharpIR
+#include <SharpIR.h>
+
+// Motor pins
+#define AIN1 7
+#define BIN1 9
+#define AIN2 6
+#define BIN2 10
+#define PWMA 5
+#define PWMB 11
+#define STBY 8
+
+// Sensor pins
+#define floor_i A0
+#define floor_d A0
+#define distance_i A0
+#define distance_r A0
+#define distance_c A0
+
+// Motor variables
+const int offsetA = 1;
+const int offsetB = 1;
+
+// Motor declaration
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+Motor motor2 = Motor(BIN1, BIN2, PWMB, offsetB, STBY);
+
+// Sensor declaration
+SharpIR left_distance(distance_i, 1080);
+SharpIR center_distance(distance_c, 1080);
+SharpIR right_distance(distance_r, 1080);
+
+
+
+void setup()
+{}
+
+void loop()
+{
+/*
+motor1.drive(VDCH);//izq
+motor2.drive(VIZQ);//dch
+*/
+
+}
